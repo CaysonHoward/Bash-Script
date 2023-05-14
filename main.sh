90e91e306de10d7cec0696b0f2c2e540ae6cff37
Cayson Howard <how20041@byui.edu>
2023-05-13 21:39:39 -0600

    Added while loop to the bash script

--- main.sh
+++ main.sh
@@ -2,12 +2,31 @@
 
 getUserName(){
     read -p "Hello! What is your name? " name
-    echo "Nice to meet you" $name "!"
-}
-
-sayHelloPython(){
-    echo "This is python! Say hello python!"
+    echo "Nice to meet you" $name"!"
+    echo -e "\nThis is python! Say hello python!"
     python3 sayHello.py $name
 }
-getUserName
-sayHelloPython
+
+userMenu() {
+    echo -e "1. Name"
+    echo -e "2. My other name"
+    echo -e "3. Python"
+    echo -e "4. Exit"
+
+    while [ "$choice" != "4" ]; do
+    read -p "Enter your choice: " choice
+    
+    if [ $choice -eq 1 ]; then
+        echo "My name is Cayson"
+    fi
+    if [ $choice -eq 2 ]; then
+        echo "My other name is Bob"
+    fi
+    if [ $choice -eq 3 ]; then
+        getUserName
+    fi
+
+    done
+}
+
+userMenu
--- sayHello.py
+++ sayHello.py
@@ -1,3 +1,4 @@
 import sys
 
+print(f"\nThis is a python program!")
 print(f"Hello {sys.argv[1]}!")
